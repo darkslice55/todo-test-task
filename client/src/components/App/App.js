98ec243794dd9bb95f643510c55fdeb403896c22
danil <danil88tmn@gmail.com>
2022-07-26 10:59:00 +0300

    Created client sceleton

---
+++ client/src/components/App/App.js
@@ -0,0 +1,9 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/react-in-jsx-scope */
+import './App.css';
+
+function App() {
+  return <div className="App">zdfsdf</div>;
+}
+
+export default App;
