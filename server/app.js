73b78b0db3e6630023057fff6755bca9ed9432a4
danil <danil88tmn@gmail.com>
2022-07-26 09:45:38 +0300

    Set up server

---
+++ server/app.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const { sequelize } = require('./db/models');
+const expressConfig = require('./config/express');
+
+const taskRouter = require('./routes/tasks.routes');
+const authRouter = require('./routes/auth.routes');
+
+const app = express();
+const PORT = process.env.PORT ?? 4000;
+
+expressConfig(app);
+
+// app.use('/tasks', taskRouter);
+// app.use('/auth', authRouter);
+
+app.listen(PORT, async () => {
+  /* eslint-disable no-console */
+  console.log('Веб-сервер слушает порт', PORT);
+
+  try {
+    await sequelize.authenticate();
+    console.log('БД-сервер подключен успешно');
+  } catch (error) {
+    console.log('БД-сервер не подключен');
+    console.log(error.message);
+  }
+});
---
+++ server/config/express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const FileStore = require('session-file-store')(session);
+const getUser = require('../middlewares/getUser');
+
+const sessionConfig = {
+  store: new FileStore(),
+  name: 'user_sid',
+  secret: process.env.SESSION_SECRET ?? 'JAShdiaf3&3',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 12,
+    httpOnly: true,
+  },
+};
+
+function expressConfig(app) {
+  app.use(express.static(path.join(__dirname, '../../frontend/build')));
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(session(sessionConfig));
+  // app.use(getUser);
+}
+
+module.exports = expressConfig;
